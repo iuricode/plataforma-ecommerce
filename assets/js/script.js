fdac2ed8e3659aed54dc6e99d554079c1651fa3f
iuricode <iuricold99@gmail.com>
2021-04-01 15:20:32 -0300

    :bookmark: v1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,18 @@
-//Animação menu responsivo
-$('.btn-menu, .btn-close').on('click', function () {
-    $('.menu').slideToggle('slow');
-});
+var cardinal = {
+  init: function() {
+      this.cacheDom();
+      this.bindEvents();
+  },
+  cacheDom: function() {
+      this.navToggle = document.querySelector('.nav-toggle');
+  },
+  bindEvents: function() {
+      this.navToggle.addEventListener('click', this.toggleNav.bind(this));
+  },
+  toggleNav: function() {
+      this.navToggle.classList.toggle('nav-open');
+      this.navToggle.parentElement.classList.toggle('nav-open');
+  }
+};
 
-$('nav ul li').click(function () {
-    $('.menu').hide();
-});
-
-const screenSize = window.screen.width;
-
-if(screenSize > 765) {
-    const menu = document.getElementById('menu-todo');
-    const menuItens = document.querySelectorAll('#menu-todo .menu ul li a')
-    var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-    
-    window.onscroll = function() {
-        scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-        if (scrollPos < 100) {
-            menu.style.position = 'static';
-            menu.style.backgroundColor = 'transparent';
-            
-            for(let i = 0; i < menuItens.length - 1; i++) {
-                menuItens[i].style.color = '#ffffffc8';
-            }
-            menuItens[menuItens.length - 1].style.color = '#7d76f8';
-            menuItens[menuItens.length - 1].style.backgroundColor = '#fff';
-        } else {
-            menu.style.position = 'fixed';
-            menu.style.backgroundColor = '#fffffff0';
-
-            for(let i = 0; i < menuItens.length - 1; i++) {
-                menuItens[i].style.color = '#7d76f8';
-            }
-
-            menuItens[menuItens.length - 1].style.color = '#fff';
-            menuItens[menuItens.length - 1].style.backgroundColor = '#7d76f8';
-        }
-    }
-    
-}
+cardinal.init();
